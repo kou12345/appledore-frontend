388ebf5b15b7f5357a758fcdd168704c45bc854b
kou12345 <hi.0412.ko.2002@gmail.com>
2024-01-15 08:57:09 +0900

    add Suspense Loading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,45 @@
 
 import { Header } from "@/components/Header";
 import { PostList } from "@/components/PostList";
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Skeleton } from "@/components/ui/skeleton";
+import { Suspense } from "react";
 
 export default async function Home() {
   return (
     <div>
       <Header />
-      <PostList />
+      <div className="grid grid-cols-6 gap-4  ">
+        <Suspense fallback={<Loading />}>
+          <PostList />
+        </Suspense>
+      </div>
     </div>
   );
 }
+
+const Loading = () => {
+  return (
+    <div className="col-start-2  col-span-4">
+      <div className="space-y-2">
+        <Card className="my-5">
+          <CardHeader>
+            <CardTitle>
+              <Skeleton className="h-4 w-3/4" />
+            </CardTitle>
+          </CardHeader>
+          <CardContent>
+            <Skeleton className="mb-1 h-4 w-2/3" />
+            <Skeleton className="mb-1 h-4 w-5/6" />
+            <Skeleton className="mb-1 h-4 w-3/4" />
+            <Skeleton className="mb-1 h-4 w-5/6" />
+          </CardContent>
+          <CardFooter className="block">
+            <Skeleton className="mb-1 h-4 w-1/2" />
+            <Skeleton className="h-4 w-1/2" />
+          </CardFooter>
+        </Card>
+      </div>
+    </div>
+  );
+};
