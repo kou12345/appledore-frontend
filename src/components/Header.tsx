a2646ae46a04c437113d98927752b89cc5d7421f
kou12345 <hi.0412.ko.2002@gmail.com>
2024-01-16 13:54:16 +0900

    Add link to homepage in Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,22 @@
+import Link from "next/link";
 import { ModeToggle } from "./ModeToggle";
 import { Search } from "./Search";
 
 export const Header = () => {
   return (
-    <div className="grid grid-cols-6 gap-4 my-5 mx-5">
-      <div className="text-center flex items-center justify-center font-bold">Appledore</div>
-      <div className=" col-start-3 col-span-2">
-        <Search />
+    <div className="mb-5">
+      <div className="grid grid-cols-6 gap-4 my-5 mx-5">
+        <div className="text-center flex items-center justify-center font-bold">
+          <Link href="/">Appledore</Link>
+        </div>
+        <div className=" col-start-3 col-span-2">
+          <Search />
+        </div>
+        <div className="col-start-6 col-span-1 flex justify-end">
+          <ModeToggle />
+        </div>
       </div>
-      <div className="col-start-6 col-span-1 flex justify-end">
-        <ModeToggle />
-      </div>
+      <hr />
     </div>
   );
 };
